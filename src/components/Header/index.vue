ad76a4486f9bca878b712c20dd9416a7fcf56dd3
abraham.zhai@cardinfolink.com <abraham.zhai@cardinfolink.com>
2020-05-23 18:48:07 +0800

    fix: li study download

---
+++ src/components/Header/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+      <div @click="handleSelectHeader(item, index)" v-for="(item, index) in headerList" :key="item+index">{{item}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LxHeader',
+  data () {
+    return {
+      msg: '头部',
+      headerList: ['综合', '用户', '话题', '专栏', 'Live', '严选经典', '电子书']
+    }
+  },
+  methods: {
+    handleSelectHeader (item, index) {
+      alert(`选择了第${index + 1}个：${item}`)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 20px;
+    background-color: #cccccc;
+    box-sizing: border-box;
+    div {
+        cursor: pointer;
+    }
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <lx-header></lx-header>
+      <div>{{msg}}</div>
+  </div>
+</template>
+<script>
+import LxHeader from './../../components/Header/index'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '李美婵'
+    }
+  },
+  components: {
+    LxHeader
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
